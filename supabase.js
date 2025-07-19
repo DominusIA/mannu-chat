f54c208a5554ef08ac21317e7b25a40c85307495
DominusIA <dominusthefuture@gmail.com>
2025-07-19 16:44:42 -0300

    Corrige instância do Supabase no supabase.js

--- supabase.js
+++ supabase.js
@@ -3,4 +3,4 @@
 const SUPABASE_URL = 'https://tqdzwjqeykmejgxfrifs.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxZHp3anFleWttZWpneGZyaWZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4OTMxNDEsImV4cCI6MjA2NjQ2OTE0MX0.lh5quH3A-wX8IhpWiVXl18xxe_7az-eZ4Pq7xHaoxOE';
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
